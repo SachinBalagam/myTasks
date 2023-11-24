1f242357a5fa396075dfb982cc73b221b51451a1
BalagamSachin <balagamsachin337@gmail.com>
2023-11-24 14:53:00 +0000

    Final Commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,9 @@
 import './App.css'
+import MyTasks from './components/myTasks/index'
 
 // These are the lists used in the application. You can move them to any component needed.
 
-const tagsList = [
-  {
-    optionId: 'HEALTH',
-    displayText: 'Health',
-  },
-  {
-    optionId: 'EDUCATION',
-    displayText: 'Education',
-  },
-  {
-    optionId: 'ENTERTAINMENT',
-    displayText: 'Entertainment',
-  },
-  {
-    optionId: 'SPORTS',
-    displayText: 'Sports',
-  },
-  {
-    optionId: 'TRAVEL',
-    displayText: 'Travel',
-  },
-  {
-    optionId: 'OTHERS',
-    displayText: 'Others',
-  },
-]
-
 // Replace your code here
-const App = () => <h1>Hello World</h1>
+const App = () => <MyTasks />
 
 export default App
